c9a43168bf2ae7f062c05fc259800e6b8f43b86c
rubemoon <aprovadosatisfy@gmail.com>
2024-09-22 20:24:34 -0300

    Refactor sign_in_user mutation to include user object in response

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -5,6 +5,7 @@ module Mutations
 
     field :token, String, null: true
     field :errors, [ String ], null: false
+    field :user, Types::UserType, null: true
 
     def resolve(email:, password:)
       user = User.find_by(email: email)
@@ -12,10 +13,10 @@ module Mutations
       if user&.valid_password?(password)
         # Ensure the user is authenticated and generate a JWT token
         token = JsonWebToken.encode(user_id: user.id)
-        { token: token, errors: [] }
+        { token: token, errors: [], user: user }
       else
         # Invalid credentials
-        { token: nil, errors: [ "Invalid email or password" ] }
+        { token: nil, errors: [ "Invalid email or password" ], user: nil }
       end
     end
   end
