66c9ec59fb87e981e1729c38524d7053f5500ebc
rubemoon <aprovadosatisfy@gmail.com>
2024-09-22 03:59:18 -0300

    Allow cookies and HTTP authentication information to be included in requests

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,6 +13,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*",
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
-      credentials: true
+      credentials: true # Allow cookies and HTTP authentication information to be included in requests
   end
 end
