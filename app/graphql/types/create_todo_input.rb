4a6b392c6d37d82681e64231768df18160177c77
rubemoon <aprovadosatisfy@gmail.com>
2024-09-22 18:13:52 -0300

    Add due_date and completed arguments to CreateTodoInput

--- app/graphql/types/create_todo_input.rb
+++ app/graphql/types/create_todo_input.rb
@@ -2,5 +2,7 @@ module Types
   class CreateTodoInput < Types::BaseInputObject
     argument :title, String, required: true
     argument :description, String, required: false
+    argument :due_date, GraphQL::Types::ISO8601DateTime, required: false
+    argument :completed, Boolean, required: false
   end
 end
