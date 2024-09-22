b493fc40b32df21e79a1c6ba6dda83395ebaa50e
rubemoon <aprovadosatisfy@gmail.com>
2024-09-22 06:11:59 -0300

    Refactor sign_up_user mutation to include token in response

--- app/graphql/mutations/sign_up_user.rb
+++ app/graphql/mutations/sign_up_user.rb
@@ -4,20 +4,26 @@ module Mutations
     argument :password, String, required: true
     argument :password_confirmation, String, required: true
 
-    field :user, Types::UserType, null: false
+    field :user, Types::UserType, null: true
+    field :token, String, null: true
     field :errors, [ String ], null: false
 
     def resolve(email:, password:, password_confirmation:)
-      existing_user = User.find_by(email: email)
-      if existing_user
-        return { user: nil, errors: [ "User already exists with this email" ] }
+      if User.exists?(email: email)
+        return { user: nil, token: nil, errors: [ "A user with this email already exists." ] }
+      end
+
+      unless password == password_confirmation
+        return { user: nil, token: nil, errors: [ "Password confirmation doesn't match." ] }
       end
 
       user = User.new(email: email, password: password, password_confirmation: password_confirmation)
+
       if user.save
-        { user: user, errors: [] }
+        token = JsonWebToken.encode(user_id: user.id)
+        { user: user, token: token, errors: [] }
       else
-        { user: nil, errors: user.errors.full_messages }
+        { user: nil, token: nil, errors: user.errors.full_messages }
       end
     end
   end
